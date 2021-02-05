9a7d03a9930e3e7493f3e1144c64507bd9f26f28
zhangjiaming <823600537@qq.com>
2021-02-06 01:13:15 +0800

    feat:完成首页

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "./App.module.css";
-import { Header, Footer, Carousel, SideMenu, ProductCollection } from "./components";
+import { Header, Footer, Carousel, SideMenu, ProductCollection, BusinessPartners } from "./components";
 import { Row, Col, Typography } from "antd";
 import { productList1, productList2, productList3 } from "./mockups";
 import sideImage from './assets/images/sider_2019_12-09.png';
@@ -47,6 +47,7 @@ function App() {
                     sideImage={sideImage3}
                     products={productList3}
                 />
+                <BusinessPartners />
             </div>
             <Footer />
         </div>
