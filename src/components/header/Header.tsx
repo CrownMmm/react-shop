6d904ff1b5bb952de96661ce57c8011c821d22f3
zhangjiaming <823600537@qq.com>
2021-02-08 01:17:00 +0800

    feat：完成路由

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,10 +3,16 @@ import styles from "./Header.module.css";
 import logo from "../../assets/logo.svg";
 import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
 import { GlobalOutlined } from "@ant-design/icons";
+import { useHistory, useLocation, useParams, useRouteMatch } from "react-router-dom";
 
 export const Header: React.FC = () => {
+    const history = useHistory();
+    const location = useLocation();
+    const params = useParams();
+    const match = useRouteMatch();
     return (
         <div className={styles["app-header"]}>
+            {/* top-header */}
             <div className={styles["top-header"]}>
                 <div className={styles.inner}>
                     <Typography.Text>让旅游更幸福</Typography.Text>
@@ -23,16 +29,18 @@ export const Header: React.FC = () => {
                         语言
                     </Dropdown.Button>
                     <Button.Group className={styles["button-group"]}>
-                        <Button>注册</Button>
-                        <Button>登陆</Button>
+                        <Button onClick={() => history.push("register")}>注册</Button>
+                        <Button onClick={() => history.push("signIn")}>登陆</Button>
                     </Button.Group>
                 </div>
             </div>
             <Layout.Header className={styles["main-header"]}>
-                <img src={logo} alt="logo" className={styles["App-logo"]} />
-                <Typography.Title level={3} className={styles.title}>
-                    React旅游网
-                </Typography.Title>
+                <span onClick={() => history.push("/")}>
+                    <img src={logo} alt="logo" className={styles["App-logo"]} />
+                    <Typography.Title level={3} className={styles.title}>
+                        React旅游网
+                    </Typography.Title>
+                </span>
                 <Input.Search
                     placeholder={"请输入旅游目的地、主题、或关键字"}
                     className={styles["search-input"]}
--- src/components/productCollection/ProductImage.tsx
+++ src/components/productCollection/ProductImage.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Image, Typography } from "antd";
+import { withRouter, RouteComponentProps, Link } from "react-router-dom";
 
-interface PropsType {
+interface PropsType extends RouteComponentProps {
     id: string | number;
     size: "large" | "small";
     imageSrc: string;
@@ -9,22 +10,34 @@ interface PropsType {
     title: string;
 }
 
-export const ProductImage: React.FC<PropsType> = ({id, size, imageSrc, price, title}) => {
+const ProductImageComponent: React.FC<PropsType> = ({
+    id,
+    size,
+    imageSrc,
+    price,
+    title,
+    history,
+    location,
+    match
+}) => {
+    // console.log(history)
+    // console.log(location)
+    // console.log(match)
     return (
-      <>
-        {size == "large" ? (
-          <Image src={imageSrc} height={285} width={490} />
-        ) : (
-          <Image src={imageSrc} height={120} width={240} />
-        )}
-        <div>
-          <Typography.Text type="secondary">
-            {title.slice(0, 25)}
-          </Typography.Text>
-          <Typography.Text type="danger" strong>
-            ¥ {price} 起
-          </Typography.Text>
-        </div>
-      </>
+        <Link to={`detail/${id}`}>
+            {size == "large" ? (
+                <Image src={imageSrc} height={285} width={490} />
+            ) : (
+                    <Image src={imageSrc} height={120} width={240} />
+                )}
+            <div>
+                <Typography.Text type="secondary">{title.slice(0, 25)}</Typography.Text>
+                <Typography.Text type="danger" strong>
+                    ¥ {price} 起
+        </Typography.Text>
+            </div>
+        </Link>
     );
-}
+};
+
+export const ProductImage = withRouter(ProductImageComponent);
