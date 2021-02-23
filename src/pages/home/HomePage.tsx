a41d9bbaa288520aa666ef4c7fdc9d399834cffa
zhangjiaming <823600537@qq.com>
2021-02-24 01:46:42 +0800

    feat:添加redux-thunk

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -7,18 +7,65 @@ import {
   ProductCollection,
   BusinessPartners,
 } from '../../components';
-import { Row, Col, Typography } from 'antd';
-import { productList1, productList2, productList3 } from './mockups';
+import { Row, Col, Typography, Spin } from 'antd';
+// import { productList1, productList2, productList3 } from './mockups';
 import sideImage from '../../assets/images/sider_2019_12-09.png';
 import sideImage2 from '../../assets/images/sider_2019_02-04.png';
 import sideImage3 from '../../assets/images/sider_2019_02-04-2.png';
 import styles from './HomePage.module.css';
 import { withTranslation, WithTranslation } from 'react-i18next';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { RootState } from '../../redux/store';
+import { giveMeDataActionCreator } from '../../redux/recommendProducts/recommendProductsActions';
 
-class HomePageComponent extends React.Component<WithTranslation> {
+const mapStateToProps = (state: RootState) => {
+  return {
+    loading: state.recommendProducts.loading,
+    error: state.recommendProducts.error,
+    productList: state.recommendProducts.productList,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    giveMeData: () => {
+      dispatch(giveMeDataActionCreator());
+    },
+  };
+};
+
+type PropsType = WithTranslation &
+  ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
+class HomePageComponent extends React.Component<PropsType> {
+  // this.props.fetchStart()
+  componentDidMount() {
+    this.props.giveMeData();
+  }
+
   render() {
     // console.log(this.props.t)
     const { t } = this.props;
+    const { productList, loading, error } = this.props;
+
+    if (loading) {
+      return (
+        <Spin
+          size="large"
+          style={{
+            marginTop: 200,
+            marginBottom: 200,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            width: '100%',
+          }}
+        />
+      );
+    }
+    if (error) {
+      return <div>网站出错：{error}</div>;
+    }
     return (
       <>
         <Header />
@@ -39,7 +86,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage}
-            products={productList1}
+            products={productList[0].touristRoutes}
           />
           <ProductCollection
             title={
@@ -48,7 +95,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage2}
-            products={productList2}
+            products={productList[1].touristRoutes}
           />
           <ProductCollection
             title={
@@ -57,7 +104,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage3}
-            products={productList3}
+            products={productList[2].touristRoutes}
           />
           <BusinessPartners />
         </div>
@@ -67,4 +114,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
   }
 }
 
-export const HomePage = withTranslation()(HomePageComponent);
+export const HomePage = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withTranslation()(HomePageComponent));
