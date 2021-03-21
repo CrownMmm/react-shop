11d1e2a523b59f59b0e3ca1f0df2627661b39043
zhangjiaming <823600537@qq.com>
2021-03-21 22:32:10 +0800

    feat:添加redux-tookit

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -19,6 +19,10 @@ import {
   ProductComments,
 } from '../../components';
 import { commentMockData } from './mockup';
+import { productDetailSlice } from '../../redux/productDetail/slice';
+import { useSelector } from '../../redux/hooks';
+import { useDispatch } from 'react-redux';
+import { stat } from 'fs';
 
 const { RangePicker } = DatePicker;
 
@@ -29,25 +33,26 @@ interface MatchParams {
 export const DetailPage: React.FC<RouteComponentProps<MatchParams>> = (
   props
 ) => {
-  //   console.log(props.history);
-  //   console.log(props.location);
-  //   console.log(props.match);
   const { touristRouteId } = useParams<MatchParams>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [product, setProduct] = useState<any>(null);
-  const [error, setError] = useState<string | null>(null);
+  //   const [loading, setLoading] = useState<boolean>(true);
+  //   const [product, setProduct] = useState<any>(null);
+  //   const [error, setError] = useState<string | null>(null);
+  const loading = useSelector((state) => state.productDetail.loading);
+  const error = useSelector((state) => state.productDetail.error);
+  const product = useSelector((state) => state.productDetail.data);
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
+      dispatch(productDetailSlice.actions.fetchStart());
       try {
         const { data } = await axios.get(
           `http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`
         );
-        setProduct(data);
-        setLoading(false);
+        dispatch(productDetailSlice.actions.fetchSuccess(data));
       } catch (error) {
-        setError(error.message);
-        setLoading(false);
+        dispatch(productDetailSlice.actions.fetchFail(error.message));
       }
     };
     fetchData();
