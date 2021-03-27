8ed8c11b7eb13005a4abadc29adfe4fe90479b5f
zhangjiaming <823600537@qq.com>
2021-03-27 18:00:56 +0800

    feat:完成购物车虚拟支付功能

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -5,13 +5,18 @@ import { Row, Col, Affix } from 'antd';
 import { ProductList, PaymentCard } from '../../components';
 import { useSelector } from '../../redux/hooks';
 import { useDispatch } from 'react-redux';
-import { clearShoppingCartItem } from '../../redux/shoppingCart/slice';
+import {
+  clearShoppingCartItem,
+  checkout,
+} from '../../redux/shoppingCart/slice';
+import { useHistory } from 'react-router-dom';
 
 export const ShoppingCartPage: React.FC = (props) => {
   const loading = useSelector((s) => s.shoppingCart.loading);
   const shoppingCartItems = useSelector((s) => s.shoppingCart.items);
   const jwt = useSelector((s) => s.user.token) as string;
   const dispatch = useDispatch();
+  const history = useHistory();
 
   return (
     <MainLayout>
@@ -38,7 +43,13 @@ export const ShoppingCartPage: React.FC = (props) => {
                       (s.discountPresent ? s.discountPresent : 1)
                   )
                   .reduce((a, b) => a + b, 0)}
-                onCheckout={() => {}}
+                onCheckout={() => {
+                  if (shoppingCartItems.length <= 0) {
+                    return;
+                  }
+                  dispatch(checkout(jwt));
+                  history.push('/placeOrder');
+                }}
                 onShoppingCartClear={() => {
                   dispatch(
                     clearShoppingCartItem({
